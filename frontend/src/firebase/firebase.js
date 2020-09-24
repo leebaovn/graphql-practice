c0082280535d8432691a6c562b5702e50bd537bb
BAO <leebao101@gmail.com>
2020-09-24 15:47:05 +0700

    add firebase

---
+++ frontend/src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCoqBUk2xnyAvDqlINX6QFL4HIr44FYs2k',
+  authDomain: 'cloud-computing-5a696.firebaseapp.com',
+  databaseURL: 'https://cloud-computing-5a696.firebaseio.com',
+  projectId: 'cloud-computing-5a696',
+  storageBucket: 'cloud-computing-5a696.appspot.com',
+  messagingSenderId: '478851109834',
+  appId: '1:478851109834:web:76a3492da7c716fce63533',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//analytics is optional for this tutoral
+const storage = firebase.storage();
+
+export { storage, firebase as default };
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -9,19 +9,59 @@ import './App.css';
 
 import MainNavigation from './components/Navigation/navigation';
 import AuthContext, { AuthProvider } from './context/auth-context';
-
+import { storage } from './firebase/firebase';
 import AuthPage from './pages/login/Auth';
 import Booking from './pages/Booking';
 import Event from './pages/Event';
 import Homepage from './components/Homepage';
 function Routes() {
   const [AuthState, AuthDispatch] = useContext(AuthContext);
-
+  const [imgUrl, setImgUrl] = useState('');
+  const [imgFile, setImgFile] = useState({});
   const { token } = AuthState;
+  const onFileSelect = (e) => {
+    if (e.target.files[0]) {
+      setImgFile(e.target.files[0]);
+    }
+  };
+  const handleUploadImage = (e) => {
+    e.preventDefault();
+    if (imgFile) {
+      const uploadTask = storage.ref(`/images/${imgFile.name}`).put(imgFile);
+      uploadTask.on(
+        'state_changed',
+        (snapShot) => {
+          console.log(snapShot);
+        },
+        (err) => {
+          console.log(err);
+        },
+        () => {
+          storage
+            .ref('images')
+            .child(imgFile.name)
+            .getDownloadURL()
+            .then((fireBaseUrl) => {
+              setImgUrl(fireBaseUrl);
+            });
+        }
+      );
+    }
+  };
   return (
     <Router>
       <MainNavigation />
       <main className='main-content'>
+        <div>
+          <h1>Upload image</h1>
+          <form onSubmit={handleUploadImage}>
+            <input type='file' name='img' id='img' onChange={onFileSelect} />
+            <button type='submit'>Upload</button>
+          </form>
+        </div>
+        <div>
+          <img src={imgUrl} alt='Img' />
+        </div>
         <Switch>
           {token && <Redirect from='/auth' to='/events' exact />}
           {!token && <Route path='/auth' component={AuthPage} />}
